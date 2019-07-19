c13f0a88df8d9d26565968257eced5f2eff0352a
Dillan Teagle <dteagle@cpicardgroup.com>
2019-07-19 14:03:46 -0500

    integrating test functionality

--- src/AWSLibrary/Keywords/session.py
+++ src/AWSLibrary/Keywords/session.py
@@ -1,6 +1,8 @@
 from robot.libraries.BuiltIn import BuiltIn
 from robot.libraries.String import String
 from robot.utils import ConnectionCache
+from robot.utils.dotdict import DotDict
+from robot.api import logger
 import boto3
 
 
@@ -14,10 +16,10 @@ class SessionManager(object):
         self.client         = None
     
 
-    def create_session(self, *args, **kwargs):
-        creds = dict(enumerate(args))
-        region = creds.get(0, kwargs.pop('region', None))
-    
+    def create_session(self, region, profile=None):
+        # creds = dict(enumerate(args))
+        # region = creds.get(0, kwargs.pop('region', None))
+
         if self.session == None:
             session = boto3.Session(
                 aws_access_key_id=self.access_key,
@@ -25,7 +27,30 @@ class SessionManager(object):
                 region_name=region
                 )
             self.session = session
+        
+        
+        self._builtin.log("Creating Session: %s" % region)
+        self._cache.register(session, alias=region)
         return self.session
+
+    def delete_session(self, region):
+        """Removes session.
+        Arguments:
+        - ``region``: A case and space insensitive string to identify the session.
+                     (Default ``region``)
+        Examples:
+        | Delete Session | REGION |
+        """
+        self._cache.switch(region)
+        index = self._cache.current_index
+        self._cache.current = self._cache._no_current
+        self._cache._connections[index - 1] = None
+        self._cache._aliases['x-%s-x' % region] = self._cache._aliases.pop(region)
+
+    def delete_all_sessions(self):
+        """ Delete All Sessions """
+        self._cache.empty_cache()
+
 
     def get_client(self, service='s3'):
         if self.client == None:
