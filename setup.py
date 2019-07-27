7258114d56521734b4c0e8c6b722460a00ceefd1
Dillan Teagle <dteagle@cpicardgroup.com>
2019-07-27 11:05:25 -0500

    add s3 allowed methods

--- setup.py
+++ setup.py
@@ -2,23 +2,24 @@ from setuptools import setup
 
 
 def readme():
-    with open('../README.md') as f:
+    with open('README.md') as f:
         README = f.read()
     return README
 
 
 setup(
     name="robotframework-aws",
-    version="0.0.1",
+    version="0.0.3",
     author="Dillan Teagle",
     author_email="softwaredeveloper@dillanteagle.me",
     description="A python package to test AWS services in Robot Framework",
     long_description=readme(),
     long_description_content_type="text/markdown",
     url="https://www.github.com/teaglebuilt/robotframework-aws",
+    license="MIT",
     classifiers={
+        "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.7"
-    },
-    install_requires=["requests"]  
+    }
 )
