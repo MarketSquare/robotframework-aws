c337fd0f66f5d944a753c3f5cb125b5e143d2778
Dillan Teagle <dteagle@cpicardgroup.com>
2019-08-05 15:48:30 -0500

    revaluate class inheritance within module

--- src/AWSLibrary/keywords/session.py
+++ src/AWSLibrary/keywords/session.py
@@ -8,32 +8,36 @@ import boto3, logging
 
 class SessionManager(object):
     
-    def __init__(self, access_key, secret_key):
+    def __init__(self):
         boto3.set_stream_logger('boto3', logging.INFO)
         self._builtin       = BuiltIn()
         self._cache         = ConnectionCache('No sessions.')
-        self.access_key     = access_key
-        self.secret_key     = secret_key
-        self.region         = None
         self.session        = None
-        self.r_session      = None
-        self.client         = None
-    
 
-    def create_session(self, region, profile=None):
+    def create_session_with_keys(self, region, access_key, secret_key):
         session = boto3.Session(
-            aws_access_key_id=self.access_key,
-            aws_secret_access_key=self.secret_key,
+            aws_access_key_id=access_key,
+            aws_secret_access_key=secret_key,
             region_name=region
         )
-        self.region = region
-        self.session = session
-        
+
         self._builtin.log("Creating Session: %s" % region)
         self._cache.register(session, alias=region)
-        return self.session
+        self.session = session
+        return session
         
-    def delete_session(self, region):
+    def create_session_with_profile(self, profile):
+        session = boto3.Session(
+            profile_name=profile
+        )
+        logger.console(session)
+        self._builtin.log("Creating Session with Profile: %s" % profile)
+        self._cache.register(session, alias=session.region_name)
+        self.session = session
+        return session
+
+
+    def delete_session(self, region, profile=None):
         """Removes session.
         Arguments:
         - ``region``: A case and space insensitive string to identify the session.
@@ -45,27 +49,19 @@ class SessionManager(object):
         index = self._cache.current_index
         self._cache.current = self._cache._no_current
         self._cache._connections[index - 1] = None
-        self._cache._aliases.pop(region)
+        self._cache._aliases.pop(region)    
 
     def delete_all_sessions(self):
         """ Delete All Sessions """
         self._cache.empty_cache()
 
-    def get_client(self, service='s3'):
-        if self.client == None:
-            client = boto3.client(service,
-                    aws_access_key_id=self.access_key,
-                    aws_secret_access_key=self.secret_key,
-                    region_name = self.region
-                )
-            self.client = client
-        return self.client
-
-    def get_resource(self, resource='s3'):
-        session = self.session
+    def get_client(self, service):
         if self.session == None:
-            session = self.create_session(self.region)
+            raise Exception("No Session")
+        client = self.session.client('s3')
+        return client
 
-        r_session = session.resource(resource)
-        self.r_session = r_session
-        return self.r_session
+    def get_resource(self, service='s3'):
+        resource = self.session.resource(service)
+        self.resource = resource
+        return resource
--- tests/robot/testsuites/test_session.robot
+++ tests/robot/testsuites/test_session.robot
@@ -1,36 +1,20 @@
 *** Settings ***
 Library  Collections
-Library  AWSLibrary  ${ACCESS_KEY}  ${SECRET_KEY}
+Library  AWSLibrary
+Resource  ../resources.robot
 
+
+*** Variable ***
+${REGION}=  us-east-1
 
 
 *** Test Case ***
-Test Create Session
-    Create Session  us-east-1
-
-Test Delete Session
-    Delete Session  us-east-1
-
-Test Delete All Sessions
-    Delete All Sessions
-
-Test Get Client
-    Get Client
-
-Test Get Resource
-    Get Resource
+Session Exists
+    Run Keyword And Expect Error
+    ...  No Session
+    ...  Get Client  ${REGION}
+    Suite Setup
+    Get Client  ${REGION}
+    Suite Teardown
 
 
-
-
-
-    # Download File  zappastaticbin  test.html  static/test.html
-    # Local File Should Exist  test.html
-    # Key Should Not Exist  zappastaticbin  test.html
-    
-    # Allowed Methods  zappastaticbin  ${crud}
-    # Get Bucket  zappastaticbin
-    # Get Object  zappastaticbin  static/js/jquery.cookie.js
-    # Key Should Not Exist  zappastaticbin  static/test.html  test.html
-    # Upload File  zappastaticbin  static/test.html  test.html
-    # Key Should Exist  zappastaticbin  static/test.html  test.html
