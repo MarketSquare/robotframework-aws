f797a6b07731c92e58c4da95dffa314335d889da
Dillan Teagle <dteagle@cpicardgroup.com>
2019-08-14 13:27:01 -0500

    deploy docs with travis

--- deploy-ghpages.py
+++ deploy-ghpages.py
@@ -8,16 +8,16 @@ from tempfile import mkdtemp
 from shutil import rmtree
 
 
-if getenv('TRAVIS_REPO_SLUG') != 'teaglebuilt/robotframework-aws':
-    print "Repo is a fork, not building docs"
+if getenv('TRAVIS_REPO_SLUG') != 'teaglebuilt/robotframework-AWS':
+    print("Repo is a fork, not building docs")
     exit()
 
 if getenv('TRAVIS_BRANCH') != 'master':
-    print "Branch is not master, not building docs"
+    print("Branch is not master, not building docs")
     exit()
 
 if getenv('TRAVIS_PULL_REQUEST') != 'false':
-    print "Pull request detected, not building docs"
+    print("Pull request detected, not building docs")
     exit()
 
 repo = check_output("git config remote.origin.url", shell=True)
@@ -29,12 +29,12 @@ rev = check_output("git rev-parse HEAD", shell=True).strip()
 dir = mkdtemp()
 check_call("git clone --branch %s %s %s" % (deploy_branch, repo, dir), shell=True)
 chdir(dir)
-check_call("python -m robot.libdoc robot_aws.Page index.html", shell=True)
-print "Docs built successfully"
+check_call("python -m robot.libdoc -n robotframework-aws src.AWSLibrary documentation.html", shell=True)
+print("Docs built successfully")
 check_call("git config user.name '%s'" % getenv('GIT_NAME'), shell=True)
 check_call("git config user.email '%s'" % getenv('GIT_EMAIL'), shell=True)
 check_call("git commit -m 'Built from %s' index.html" % rev, shell=True)
 check_call("git push -q %s %s" % (deploy_url, deploy_branch), shell=True)
 chdir(getenv('TRAVIS_BUILD_DIR'))
 rmtree(dir)
-print "Docs pushed successfully"
+print("Docs pushed successfully")
