fde138215b8a446aa6a086540c4a76f18edabed9
EMEA-MEDIA\scabra02 <samuelpierri.cabral@merkle.com>
2024-06-12 18:10:35 +0100

    Update documentation

--- src/AWSLibrary/keywords/s3.py
+++ src/AWSLibrary/keywords/s3.py
@@ -13,12 +13,13 @@ class S3Keywords(LibraryComponent):
         """
         *DEPRECATED - this keyword will be removed in version 2.0.0* use `S3 Create Bucket` instead
 
-        Creates S3 Bucket with the name given @param: ```bucket```
-            and uses the optionally provided @param: ```endpoint_url```.
-            If Bucket already exists, an exception will be thrown.
+        Creates S3 Bucket with the given bucket name
 
-        Example:
-        | Create Bucket | randomBucketName |
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+
+        *Examples:*
+        | Create bucket _name | bucket_name |
         """
         logger.warn("Keyword 'Create Bucket' is deprecated. Use keyword 'S3 Create Bucket' instead")
         client = self.state.session.client('s3', endpoint_url=endpoint_url)
@@ -41,11 +42,19 @@ class S3Keywords(LibraryComponent):
         """
         *DEPRECATED - this keyword will be removed in version 2.0.0* use `S3 List Objects` instead
 
-        List Objects
-        Requires:   @param: ```bucket``` which is the bucket name
-        Optional:   @param: ```prefix``` which limits the response to keys that begin with the specified prefix.
-            Example:
-            | List Objects | bucket | key |
+        List objects in a bucket. (up to 1,000) each request
+
+        If prefix is informed will list only the files that start with this string,
+        could be used to list only the files inside a folder for example.
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``Prefix`` | <str> prefix of the filepath. |
+
+        *Examples:*
+        | List Objects | bucket_name |
+        | List Objects | bucket_name | folder_name |
+        | List Objects | bucket_name | folder_name/start_of_the_filename |
         """
         logger.warn("Keyword 'List Objects' is deprecated. Use keyword 'S3 List Objects' instead")
         client = self.state.session.client('s3')
@@ -66,12 +75,15 @@ class S3Keywords(LibraryComponent):
         """
         *DEPRECATED - this keyword will be removed in version 2.0.0* use `S3 Delete File` instead
 
-        Delete File
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Delete File | bucket | key |
+        Deletes the file from a bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete filepath. |
+
+        *Examples:*
+        | Delete File | bucket_name | file.txt |
+        | Delete File | bucket_name | folder/file.txt |
         """
         logger.warn("Keyword 'Delete File' is deprecated. Use keyword 'S3 Delete File' instead")
         client = self.state.session.client('s3', endpoint_url=endpoint_url)
@@ -89,13 +101,16 @@ class S3Keywords(LibraryComponent):
         """
         *DEPRECATED - this keyword will be removed in version 2.0.0* use `S3 Download File` instead
 
-        Downloads File from S3 Bucket
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```path``` which is the local path of file and it´s name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Download File | bucket | key | file.txt |
+        Download file from a bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete filepath. |
+        | ``path`` | <str> Complete local filepath. |
+
+        *Examples:*
+        | Download File | bucket_name | s3_file.txt | ${OUTPUTDIR}/file.txt |
+        | Download File | bucket_name | folder/s3_file.txt | ${OUTPUTDIR}/file.txt |
         """
         logger.warn("Keyword 'Download File' is deprecated. Use keyword 'S3 Download File' instead")
         client = self.state.session.client('s3', endpoint_url=endpoint_url)
@@ -109,13 +124,16 @@ class S3Keywords(LibraryComponent):
         """
         *DEPRECATED - this keyword will be removed in version 2.0.0* use `S3 Upload File` instead
 
-        Uploads File from S3 Bucket
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```path``` which is the local path of file.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Upload File | bucket | path | key |
+        Upload a file to the bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+        | ``path`` | <str> Complete local filepath. |
+
+        *Examples:*
+        | Upload File | bucket_name | s3_file.txt | ${CURDIR}/file.txt |
+        | Upload File | bucket_name | folder/s3_file.txt | ${CURDIR}/file.txt |
         """
         logger.warn("Keyword 'Upload File' is deprecated. Use keyword 'S3 Upload File' instead")
         client = self.state.session.client('s3', endpoint_url=endpoint_url)
@@ -134,13 +152,15 @@ class S3Keywords(LibraryComponent):
         """
         *DEPRECATED - this keyword will be removed in version 2.0.0* use `S3 Key Should Exist` instead
 
-        Key Should Exist
-        Description: Checks to see if the file on the s3 bucket exist. If so, the keyword will not fail.
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Key Should Exist | bucket | key |
+        Check if the s3 object exist inside the bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+
+        *Examples:*
+        | Key Should Exist | bucket_name | s3_file.txt |
+        | Key Should Exist | bucket_name | folder/s3_file.txt |
         """
         logger.warn("Keyword 'Key Should Exist' is deprecated. Use keyword 'S3 Key Should Exist' instead")
         client = self.state.session.client("s3", endpoint_url=endpoint_url)
@@ -155,13 +175,15 @@ class S3Keywords(LibraryComponent):
         """
         *DEPRECATED - this keyword will be removed in version 2.0.0* use `S3 Key Should Not Exist` instead
 
-        Verifies Key on S3 Bucket Does Not Exist
-        Description: Checks to see if the file on the s3 bucket exist. If so, the keyword will fail.
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Key Should Not Exist | bucket | key |
+        Check if the s3 object not exist inside the bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+
+        *Examples:*
+        | Key Should Not Exist | bucket_name | s3_file.txt |
+        | Key Should Not Exist | bucket_name | folder/s3_file.txt |
         """
         logger.warn("Keyword 'Key Should Not Exist' is deprecated. Use keyword 'S3 Key Should Not Exist' instead")
         client = self.state.session.client('s3', endpoint_url=endpoint_url)
@@ -176,36 +198,45 @@ class S3Keywords(LibraryComponent):
     # end of deprecated keywords
 
     @keyword('S3 Create Bucket')
-    def s3_create_bucket(self, bucket, endpoint_url=None):
-        """ Creates S3 Bucket with the name given @param: ```bucket```
-            and uses the optionally provided @param: ```endpoint_url```.
-            If Bucket already exists, an exception will be thrown.
+    def s3_create_bucket(self, bucket):
+        """ Creates S3 Bucket with the given bucket name
 
-        Example:
-        | Create Bucket | randomBucketName |
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+
+        *Examples:*
+        | S3 Create bucket | bucket_name |
         """
-        client = self.state.session.client('s3', endpoint_url=endpoint_url)
+        client = self.state.session.client('s3')
         try:
             client.create_bucket(Bucket=bucket)
             logger.debug(f"Created new bucket: {bucket}")
         except botocore.exceptions.ClientError as e:
             error_code = e.response["Error"]["Code"]
             if error_code == "BucketAlreadyExists":
-                self.logger.debug(f"Keyword Failed: {error_code}")
+                logger.debug(f"Keyword Failed: {error_code}")
                 raise Exception(f"Keyword Failed: {error_code}")
             elif error_code == 'BucketAlreadyOwnedByYou':
-                self.logger.debug("Bucket Already Exists")
+                logger.debug("Bucket Already Exists")
                 raise Exception("Bucket Already Exists")
             else:
                 Exception(f"Error Code: {e.response['Error']['Code']}")
 
     @keyword('S3 List Objects')
     def s3_list_objects(self, bucket, prefix=""):
-        """ List Objects
-        Requires:   @param: ```bucket``` which is the bucket name
-        Optional:   @param: ```prefix``` which limits the response to keys that begin with the specified prefix.
-            Example:
-            | List Objects | bucket | key |
+        """ List objects in a bucket. (up to 1,000) each request
+
+        If prefix is informed will list only the files that start with this string,
+        could be used to list only the files inside a folder for example.
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``Prefix`` | <str> prefix of the filepath. |
+
+        *Examples:*
+        | S3 List Objects | bucket_name |
+        | S3 List Objects | bucket_name | folder_name |
+        | S3 List Objects | bucket_name | folder_name/start_of_the_filename |
         """
         client = self.state.session.client('s3')
         try:
@@ -221,15 +252,18 @@ class S3Keywords(LibraryComponent):
             raise Exception(e)
 
     @keyword('S3 Delete File')
-    def s3_delete_file(self, bucket, key, endpoint_url=None):
-        """ Delete File
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Delete File | bucket | key |
+    def s3_delete_file(self, bucket, key):
+        """ Deletes the file from a bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+
+        *Examples:*
+        | S3 Delete File | bucket_name | file.txt |
+        | S3 Delete File | bucket_name | folder/file.txt |
         """
-        client = self.state.session.client('s3', endpoint_url=endpoint_url)
+        client = self.state.session.client('s3')
         try:
             response = client.delete_object(
                 Bucket=bucket,
@@ -240,34 +274,40 @@ class S3Keywords(LibraryComponent):
             raise Exception(e)
 
     @keyword('S3 Download File')
-    def s3_download_file_from_s3(self, bucket, key, path, endpoint_url=None):
-        """ Downloads File from S3 Bucket
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```path``` which is the local path of file and it´s name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Download File | bucket | key | file.txt |
+    def s3_download_file_from_s3(self, bucket, key, local_filepath):
+        """ Download file from a bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+        | ``local_path`` | <str> Complete local filepath. |
+
+        *Examples:*
+        | S3 Download File | bucket_name | s3_file.txt | ${OUTPUTDIR}/file.txt |
+        | S3 Download File | bucket_name | folder/s3_file.txt | ${OUTPUTDIR}/file.txt |
         """
-        client = self.state.session.client('s3', endpoint_url=endpoint_url)
+        client = self.state.session.client('s3')
         try:
-            client.download_file(bucket, key, path)
+            client.download_file(bucket, key, local_filepath)
         except botocore.exceptions.ClientError as e:
             raise Exception(e)
 
     @keyword('S3 Upload File')
-    def s3_upload_file(self, bucket, key, path, endpoint_url=None):
-        """ Uploads File from S3 Bucket
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```path``` which is the local path of file.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Upload File | bucket | path | key |
+    def s3_upload_file(self, bucket, key, local_path):
+        """ Upload a file to the bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+        | ``local_path`` | <str> Complete local filepath. |
+
+        *Examples:*
+        | S3 Upload File | bucket_name | s3_file.txt | ${CURDIR}/file.txt |
+        | S3 Upload File | bucket_name | folder/s3_file.txt | ${CURDIR}/file.txt |
         """
-        client = self.state.session.client('s3', endpoint_url=endpoint_url)
+        client = self.state.session.client('s3')
         try:
-            client.upload_file(path, bucket, key)
+            client.upload_file(local_path, bucket, key)
             response = client.head_object(
                 Bucket=bucket,
                 Key=key
@@ -277,33 +317,36 @@ class S3Keywords(LibraryComponent):
             raise Exception(e)
 
     @keyword('S3 Key Should Exist')
-    def s3_key_should_exist(self, bucket, key, endpoint_url=None):
-        """ Key Should Exist
-        Description: Checks to see if the file on the s3 bucket exist. If so, the keyword will not fail.
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Key Should Exist | bucket | key |
+    def s3_key_should_exist(self, bucket, key):
+        """ Check if the s3 object exist inside the bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+
+        *Examples:*
+        | S3 Key Should Exist | bucket_name | s3_file.txt |
+        | S3 Key Should Exist | bucket_name | folder/s3_file.txt |
         """
-        client = self.state.session.client("s3", endpoint_url=endpoint_url)
+        client = self.state.session.client("s3")
         try:
             client.head_object(Bucket=bucket, Key=key)
         except botocore.exceptions.ClientError:
             raise Exception(f"Key: {key} does not exist inside {bucket}")
-        return True
 
     @keyword('S3 Key Should Not Exist')
-    def s3_key_should_not_exist(self, bucket, key, endpoint_url=None):
-        """ Verifies Key on S3 Bucket Does Not Exist
-        Description: Checks to see if the file on the s3 bucket exist. If so, the keyword will fail.
-        Requires:   @param ```bucket``` which is the bucket name:
-                    @param: ```key``` which is the bucket location/path name.
-                    @param: ```endpoint_url``` optionally override the s3 endpoint url
-            Example:
-            | Key Should Not Exist | bucket | key |
+    def s3_key_should_not_exist(self, bucket, key):
+        """ Check if the s3 object not exist inside the bucket
+
+        | =Arguments= | =Description= |
+        | ``bucket`` | <str> The bucket name. |
+        | ``key`` | <str> complete s3 filepath. |
+
+        *Examples:*
+        | S3 Key Should Not Exist | bucket_name | s3_file.txt |
+        | S3 Key Should Not Exist | bucket_name | folder/s3_file.txt |
         """
-        client = self.state.session.client('s3', endpoint_url=endpoint_url)
+        client = self.state.session.client('s3')
         try:
             response = client.head_object(Bucket=bucket, Key=key)
             if response['ResponseMetadata']['HTTPStatusCode'] == 200:
