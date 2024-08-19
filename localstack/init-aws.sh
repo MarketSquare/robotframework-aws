78cf133473161f37a3d1775120124e3296ae172f
EMEA-MEDIA\scabra02 <samuelpierri.cabral@merkle.com>
2024-08-19 17:01:04 +0100

    test to create dynamo table

--- localstack/init-aws.sh
+++ localstack/init-aws.sh
@@ -4,4 +4,10 @@
 awslocal sqs create-queue --queue-name test-queueName
 awslocal s3api create-bucket --bucket test-bucket
 awslocal s3api create-bucket --bucket test-bucket-2
+awslocal dynamodb create-table \
+    --table-name global01 \
+    --key-schema AttributeName=id,KeyType=HASH \
+    --attribute-definitions AttributeName=id,AttributeType=S \
+    --billing-mode PAY_PER_REQUEST \
+    --region us-east-1
 echo succesfully created services
